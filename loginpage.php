3d4f963bf3d1b16d23f03af16ed0864ecb608c7b
paulsen1 <37793184+paulsen1@users.noreply.github.com>
2018-05-20 23:08:08 +1000

    Add files via upload
    login stuff

---
+++ loginpage.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+	$con = mysqli_connect("localhost", "admin", "password", "design_market");
+	$user = mysqli_real_escape_string($con, $_POST['username']);
+	$sql = "SELECT * FROM users WHERE username = '$user'";
+	$result=$con->query($sql);
+	$array = $result->fetch_assoc();
+	if (password_verify($_POST['password'], $array['hash'])) {
+		$_SESSION['username'] = $user;
+		$_SESSION['password'] = $array['hash'];
+?>
+<script>
+	window.location.replace("./home.php");
+</script>
+<?php
+	} else {
+		echo "Login failed";
+	}
+?>
+<script>
+	window.location.replace("./login.php");
+</script>
