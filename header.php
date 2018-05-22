9306308a1ec47772a021af629d073833fcc9a7cb
paulsen1 <37793184+paulsen1@users.noreply.github.com>
2018-05-22 23:10:45 +1000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html>
+    
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="images/homeButton.png" />
+    
+    <head>
+        
+        <link rel="icon" type="image/png" href="images/logo.png">
+        
+        <!-- Space for scripts -->
+        <script>
+        
+        </script>
+        
+    </head>
+    
+    <body>
+    
+        <header>
+        
+            <div class="content">
+                
+                <nav id="navigationMenu">
+        
+                    <li><a href="home.php"  id="homeButton"><img src="images/logo.png" width="70"></a></li>
+            
+                    <ul> 
+                        <li><a href="marketplace.php">Marketplace</a></li> 
+                        <li><a href="support.php">Support</a></li> 
+                    </ul>
+                
+                </nav>
+                
+                <!-- Needs php: When User is logged in, should show Profile button instead of Login/Register -->
+                
+                <?php 
+                if (!isset($_SESSION['username']) || !isset($_SESSION['password'])) { ?>
+                
+                <nav id="loginButtons">
+                    
+                    <ul>
+                        <li><a id="loginButton" href="login.php">Login</a></li> 
+                        <li><a id="registerButton" href="register.php">Sign Up</a></li> 
+                    </ul>
+                </nav>
+                
+                <?php 
+                } else { 
+                            echo $_SESSION['username'];
+                } ?>
+                
+            </div>
+        
+        </header>
+        
+    </body>
+
+</html>
---
+++ support.php
@@ -0,0 +1,35 @@
+<?php 
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="images/homeButton.png" />
+    
+    <head>
+        
+        <title>Design Market - Home</title>
+        
+        <link rel="icon" type="image/png" href="images/logo.png">
+        
+        <!-- Space for scripts -->
+        <script></script>
+        
+    </head>
+    
+    <body>
+    
+        <?php include('header.php'); ?>
+        
+        <div id="container">
+        
+            
+            
+        </div>
+        
+        <?php include('footer.php'); ?>
+    
+    </body>
+    
+</html>
