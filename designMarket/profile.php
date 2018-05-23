fe551196bf2010bf8a931a7267911f4ae6e11b44
Ben Fowler <ben.r.fowler@hotmail.com>
2018-05-23 16:07:58 +1000

    added profile

---
+++ designMarket/profile.php
@@ -0,0 +1,41 @@
+<?php 
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="images/homeButton.png" />
+    
+    <head>
+        
+        <title>Design Market - Profile</title>
+        
+        <link rel="icon" type="image/png" href="images/logo.png">
+        
+        <!-- Space for scripts -->
+        <script></script>
+        
+    </head>
+    
+    <body>
+    
+        <?php include('header.php'); ?>
+        
+        <div id="profileContainer">
+        
+            <div id="profileBanner">
+            </div>
+            
+            <div id="profileBanner">
+                <div id="profilePicture"></div>
+            </div>
+            
+            
+        </div>
+        
+        <?php include('footer.php'); ?>
+    
+    </body>
+    
+</html>
