4b5ff2e73cad660b94151917ec89e99a4ddcfffa
Ben Fowler <ben.r.fowler@hotmail.com>
2018-05-24 21:37:47 +1000

    changes to profilePortfolio + css + changed footer title

---
+++ designMarket/profilePortfolio.php
@@ -0,0 +1,55 @@
+<?php 
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="images/homeButton.png" />
+    
+    <head>
+        
+        <title>Design Market - Profile</title>
+        
+        <link rel="icon" type="image/png" href="images/logo.png">
+        
+        <!-- Space for scripts -->
+        <script></script>
+        
+    </head>
+    
+    <body>
+    
+        <?php include('header.php'); ?>
+            
+        <div id="profileContainer">
+            
+            <div id="portfolioHeader">
+                
+                <h2>Username's Portfolio</h2>
+                
+            </div>
+            
+            <div id="profilePortfolio">
+                
+                <ul>
+                    <li><img src="images/exampleLogo4.png" width="200"><img src="images/fourStars.png" width="200"></li>
+                    <li><img src="images/exampleLogo4.png" width="200"><img src="images/fourStars.png" width="200"></li>
+                    <li><img src="images/exampleLogo4.png" width="200"><img src="images/fourStars.png" width="200"></li>
+                    <li><img src="images/exampleLogo4.png" width="200"><img src="images/fourStars.png" width="200"></li>
+                    <li><img src="images/exampleLogo4.png" width="200"><img src="images/fourStars.png" width="200"></li>
+                    <li><img src="images/exampleLogo4.png" width="200"><img src="images/fourStars.png" width="200"></li>
+                    <li><img src="images/exampleLogo4.png" width="200"><img src="images/fourStars.png" width="200"></li>
+                    <li><img src="images/exampleLogo4.png" width="200"><img src="images/fourStars.png" width="200"></li>
+                    <li><img src="images/exampleLogo4.png" width="200"><img src="images/fourStars.png" width="200"></li>
+                </ul>
+                
+            </div>
+            
+        </div>
+        
+        <?php include('footer.php'); ?>
+    
+    </body>
+    
+</html>
