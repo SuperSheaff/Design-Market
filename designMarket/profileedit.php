cba8290f9a2ad6585bc5c0723a75437e7c1b32fe
paulsen1 <37793184+paulsen1@users.noreply.github.com>
2018-05-25 14:38:27 +1000

    edit the profile

---
+++ designMarket/profileedit.php
@@ -0,0 +1,19 @@
+<?
+	$con = mysqli_connect("localhost", "admin", "password", "design_market");
+	$id = $_SESSION['id'];
+	mysqli_real_escape_string($con, $_POST['description']);
+
+	$source = "./uploads/" . basename($_FILES["uploadProfilePicture"]["name"]);
+	$error = 1;
+	if ($_FILES["uploadProfilePicture"]["size"] > 100000) {
+	    $error = 3;
+	}
+	if (file_exists($source)) {
+	    $error = 2;
+	}
+	if ($error == 1) {
+		move_uploaded_file($_FILES["uploadProfilePicture"]["tmp_name"], $source);
+	}
+
+	$sql = "UPDATE users SET description='$description', image='$source'";
+?>
